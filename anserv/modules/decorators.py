da8aa9193640b7ed22ef5792ffa8f32e772e67ee
Piotr Mitros <pmitros@mitx.mit.edu>
2012-12-30 08:48:36 -0500

    Minor cleanup: Function specs are data and not a big if/else

--- anserv/modules/decorators.py
+++ anserv/modules/decorators.py
@@ -25,41 +25,38 @@ def event_handler(queued=True, per_user=False, per_resource=False, single_proces
         return func
     return event_handler_factory
 
+funcspecs = [ (['db','params'], 'global'), 
+              (['db','user','params'], 'user') ]
+
+def register_handler(cls, category, name, docstring, f):
+     if cls not in ['view', 'query']:
+         raise ValueError("We can only register views and queries")
+     if not name: 
+         name = str(f.func_name)
+     if not docstring: 
+         docstring = str(f.func_doc)
+     if not category:
+         for (params, cat) in funcspecs:
+             if inspect.getargspec(f).args == params:
+                 category = cat
+     if not category: 
+         raise ValueError('Function arguments do not match recognized type. Explicitly set category in decorator.')
+     if category not in request_handlers[cls]:
+         request_handlers[cls][category]={}
+     if name in request_handlers[cls][category]:
+         raise KeyError(name+" already in "+category)
+     request_handlers[cls][category][name] = {'function': f, 'name': name, 'doc': docstring}
+
 def view(category = None, name = None, docstring = None):
-    def view_factory(a):
-        ## Register function in the registry
-        registered_name = name
-        doc = docstring
-        cat = category
-        if not registered_name:
-            registered_name = str(a.func_name)
-        if not doc: 
-            doc = str(a.func_doc)
-
-        if not cat:
-            if inspect.getargspec(a).args == ['db','params']:
-                cat = 'global'
-            else:
-                raise ValueError('Function arguments do not match recognized type. Explicitly set category in decorator.')
-
-        if cat not in request_handlers['view']:
-            request_handlers['view'][cat]={}
-        if registered_name in request_handlers['view'][cat]:
-            raise KeyError(registered_name+" already in "+cat)
-        request_handlers['view'][cat][registered_name] = {'function': a, 'name': registered_name, 'doc': doc}
-        return a
+    def view_factory(f):
+        register_handler('view',category, name, docstring, f)
+        return f
     return view_factory
 
-def query(category, name):
+def query(category = None, name = None, docstring = None):
     ''' 
     ''' 
-    def query_factory(a):
-        ## Register function in the registry
-        if category not in request_handlers['query']:
-            request_handlers['query'][category]={}
-        if name in request_handlers['query'][category]:
-            raise KeyError(name+" already in "+category)
-        request_handlers['query'][category][name] = a
-        
-        return a
+    def query_factory(f):
+        register_handler('query',category, name, docstring, f)
+        return f
     return query_factory
