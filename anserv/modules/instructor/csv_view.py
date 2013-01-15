b9f7d381a9abd3cdde94b9d1b4ccf1b8cb10fc4a
Piotr Mitros <pmitros@mitx.mit.edu>
2013-01-15 09:37:25 -0500

    Syncing to work machine/first 5 lines of CSV export

---
+++ anserv/modules/instructor/csv_view.py
@@ -0,0 +1,8 @@
+def return_csv(fs):
+    fs.open('results.csv')
+    writer = csv.writer(response, dialect='excel', quotechar='"', quoting=csv.QUOTE_ALL)
+    writer.writerow(datatable['header'])
+    for datarow in datatable['data']:
+        encoded_row = [unicode(s).encode('utf-8') for s in datarow]
+        writer.writerow(encoded_row)
+    return ""
