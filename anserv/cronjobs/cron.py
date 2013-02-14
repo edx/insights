a1347ea920e60b6079bf96f20b64aa93b8a2ead4
Vik Paruchuri <vik.paruchuri@gmail.com>
2013-02-13 23:38:33 -0500

    Work on incorporating new cronjobs lib

--- anserv/cronjobs/cron.py
+++ anserv/cronjobs/cron.py
@@ -1,20 +1,22 @@
-import atexit
 import logging
-import os
-import sys
 import imp
-import tempfile
 
 from django.conf import settings
 from django.utils.importlib import import_module
+from django.utils import timezone
+import datetime
 
 import cronjobs
+from models import CronLock
+from django.db import transaction
 
 log = logging.getLogger('cron')
 
 LOCK = getattr(settings, 'CRONJOB_LOCK_PREFIX', 'lock')
+LOCK_TIMEOUT = getattr(settings, 'LOCK_TIMEOUT', 5 * 60 * 60)
 
-def run_single_job(script, *args):
+def run_all_jobs():
+    transaction.commit_unless_managed()
     for app in settings.INSTALLED_APPS:
         try:
             app_path = import_module(app).__path__
@@ -26,35 +28,37 @@ def run_single_job(script, *args):
         except ImportError as e:
             continue
 
-        import_module('%s.cron' % app)
-
+    import_module('%s.cron' % app)
     registered = cronjobs.registered
+    for script in registered:
+        # Acquire lock if needed.
+            if script in cronjobs.registered_lock:
+                lock_name = 'django_cron.%s.%s' % (LOCK, script)
+                try:
+                    cron_locks = CronLock.objects.filter(name=lock_name).order_by("-date_created")
+                    lock_count = cron_locks.count()
+                    has_lock = False
+                    #Remove extraneous locks
+                    if lock_count>=1:
+                        for lock in cron_locks[1:]:
+                            lock.delete()
+                        transaction.commit_unless_managed()
+                        lock=cron_locks[0]
+                        #Remove lock if it has timed out
+                        if lock.date_modified < (timezone.now() - datetime.timedelta(seconds=LOCK_TIMEOUT)):
+                            lock.delete()
+                        else:
+                            has_lock = True
+                        transaction.commit_unless_managed()
 
-    if script not in registered:
-        log.error("Cron called with unrecognized command: %s %s" %
-                  (script, args))
-        print 'Unrecognized name: %s' % script
-        sys.exit(1)
+                    if not has_lock:
+                        run_single_job(script, registered, )
 
-    # Acquire lock if needed.
-    if script in cronjobs.registered_lock:
-        filename = os.path.join(tempfile.gettempdir(),
-                                'django_cron.%s.%s' % (LOCK, script))
-        try:
-            fd = os.open(filename, os.O_CREAT|os.O_EXCL)
+                except:
+                    pass
 
-            def register():
-                os.close(fd)
-                os.remove(filename)
 
-            atexit.register(register)
-        except OSError:
-            msg = ("Script run multiple times. If this isn't true, delete "
-                   "`%s`." % filename)
-            log.error(msg)
-            sys.stderr.write(msg + "\n")
-            sys.exit(1)
-
+def run_single_job(script, registered, *args):
     log.info("Beginning job: %s %s" % (script, args))
     registered[script](*args)
     log.info("Ending job: %s %s" % (script, args))
---
+++ anserv/cronjobs/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from datetime import datetime
+
+class CronLock(models.Model):
+    name = models.CharField(max_length=100)
+
+    # time between job runs (in seconds) // default: 1 day
+    date_created = models.DateTimeField(auto_now_add = True)
+    date_modified = models.DateTimeField(auto_now = True)
