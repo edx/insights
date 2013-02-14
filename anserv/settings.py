158e471d8978777781eb7bf2f1db56b0d379a484
Vik Paruchuri <vik@edx.org>
2013-02-14 10:01:28 -0500

    Revert system specific settings

--- anserv/settings.py
+++ anserv/settings.py
@@ -22,7 +22,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': { ## Main analytics read replica
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/home/vik/mitx_all/db/mitx.db',                      # Or path to database file if using sqlite3.
+        'NAME': '../../db/mitx.db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
