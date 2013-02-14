7adcf50f27e805931e7bb4c39fa07fa346710acf
Vik Paruchuri <vik@edx.org>
2013-02-14 15:57:50 -0500

    Fix up mixpanel course tracking

--- anserv/modules/mixpanel/generic_event_handlers.py
+++ anserv/modules/mixpanel/generic_event_handlers.py
@@ -3,7 +3,7 @@ from modules.decorators import view, query, event_handler
 import re
 
 SINGLE_PAGES_TO_TRACK = ['/', '/dashboard', '/create_account']
-REGEX_PAGES_TO_TRACK = ['/course', '/about']
+COURSE_PAGES_TO_TRACK = ['/courses', '/about']
 @event_handler()
 def single_page_track_event(fs, db, response):
     for resp in response:
@@ -12,9 +12,13 @@ def single_page_track_event(fs, db, response):
             track_event_mixpanel(resp['event_type'],{'user' : user, 'distinct_id' : user})
 
 @event_handler()
-def regex_track_event(fs,db,response):
-    for rep in response:
-        for regex in REGEX_PAGES_TO_TRACK:
+def course_track_event(fs,db,response):
+    for resp in response:
+        for regex in COURSE_PAGES_TO_TRACK:
             match = re.search(regex, resp['event_type'])
+            user = resp["username"]
             if match is not None:
-                track_event_mixpanel(regex,{'user' : user, 'distinct_id' : user, 'full_url' : resp['event_type']})
+                split_url = resp['event_type'].split("/")
+                org = split_url[2]
+                course = split_url[3]
+                track_event_mixpanel(regex,{'user' : user, 'distinct_id' : user, 'full_url' : resp['event_type'], 'course' : course, 'org' : org})
