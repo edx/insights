6a6f13e5eb0ef551903f769500f251493cc147da
Vik Paruchuri <vik@edx.org>
2013-02-21 17:27:58 -0500

    Change module paths, add in requirements

--- anserv/aws.py
+++ anserv/aws.py
@@ -8,9 +8,14 @@ with open(ENV_ROOT / "auth.json") as auth_file:
     AUTH_TOKENS = json.load(auth_file)
 
 MIXPANEL_KEY = AUTH_TOKENS.get("MIXPANEL_KEY","")
+
 LOG_READ_DIRECTORY = "/mnt/edx-all-tracking-logs"
 DIRECTORIES_TO_READ = ["prod-edx-00{0}".format(i) for i in xrange(0,9)]
+MODULE_RESOURCE_STATIC = '/opt/wwc/modules/static/resource'
+
 DEFAULT_DATABASE = AUTH_TOKENS.get("DATABASES",DATABASES)
 DATABASES['default'] = DEFAULT_DATABASE['default']
+
+
 
 ROOT_URLCONF = 'urls'
