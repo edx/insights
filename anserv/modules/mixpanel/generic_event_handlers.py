8c27e97310da0579cf71a728471f876f37e8795f
Vik Paruchuri <vik@edx.org>
2013-02-21 11:18:42 -0500

    Add in more tracking events

--- anserv/modules/mixpanel/generic_event_handlers.py
+++ anserv/modules/mixpanel/generic_event_handlers.py
@@ -2,12 +2,16 @@ from modules.mixpanel.mixpanel import track_event_mixpanel
 from modules.decorators import view, query, event_handler
 import re
 import logging
-SINGLE_PAGES_TO_TRACK = ['/', '/dashboard', '/create_account']
+SINGLE_PAGES_TO_TRACK = ['/', '/dashboard', '/create_account', 'page_close']
 COURSE_PAGES_TO_TRACK = ['/courses', '/about']
+VIDEO_EVENTS_TO_TRACK = ['play_video', 'pause_video']
+PROBLEM_EVENTS_TO_TRACK = ['problem_check', 'problem_show', 'show_answer', 'save_problem_check', 'reset_problem']
+BOOK_EVENTS_TO_TRACK = ['book']
+
 @event_handler()
 def single_page_track_event(fs, db, response):
     for resp in response:
-        if resp['event_type'] in  SINGLE_PAGES_TO_TRACK:
+        if resp['event_type'] in  SINGLE_PAGES_TO_TRACK + BOOK_EVENTS_TO_TRACK + PROBLEM_EVENTS_TO_TRACK + VIDEO_EVENTS_TO_TRACK:
             user = resp["username"]
             host = resp['host']
             agent = resp['agent']
@@ -26,3 +30,5 @@ def course_track_event(fs,db,response):
                 host = resp['host']
                 agent = resp['agent']
                 track_event_mixpanel(regex,{'user' : user, 'distinct_id' : user, 'full_url' : resp['event_type'], 'course' : course, 'org' : org, 'host' : host, 'agent' : agent})
+
+
