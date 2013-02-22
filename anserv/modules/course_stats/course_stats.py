f3fb91ed88c1f680f5c017019b5ebf5cab4ccc33
Vik Paruchuri <vik@edx.org>
2013-02-22 16:00:08 -0500

    Simple user count per course table

--- anserv/modules/course_stats/course_stats.py
+++ anserv/modules/course_stats/course_stats.py
@@ -4,10 +4,10 @@ from django.contrib.auth.models import User
 from courseware.models import StudentModule
 import json
 from django.conf import settings
-import dummy_values
 import logging
 from django.utils import timezone
 import datetime
+from modules.user_stats import user_stats
 
 log=logging.getLogger(__name__)
 import re
@@ -26,7 +26,7 @@ def modules_accessed_in_course_count_query(fs, db, course, params):
 def problems_tried_in_course_count_query(fs, db, course, params):
     return StudentModule.objects.filter(course_id=course,module_type="problem").count()
 
-@query('course', 'video_watch_count)
+@query('course', 'video_watch_count')
 def videos_watched_in_course_count_query(fs, db, course, params):
     return StudentModule.objects.filter(course_id=course,module_type="video").count()
 
@@ -45,3 +45,19 @@ def problems_tried_in_course_count_view(fs, db, course, params):
 @view('course', 'video_watch_count')
 def videos_watched_in_course_count_view(fs, db, course, params):
     return str(videos_watched_in_course_count_query(fs,db,course,params)) + "videos watched in the course."
+
+@query('global', 'users_per_course_count')
+def users_per_course_count_query():
+    query_string = "SELECT course_id, COUNT(DISTINCT user_id) AS count FROM student_courseenrollment GROUP BY course_id"
+    return user_stats.query_results(query_string)
+
+@view('global', 'users_per_course')
+def users_per_course_count_view():
+    query_data = users_per_course_count_query()
+    html_string="<table><tr><td>Course ID</td><td>Count</td></tr>"
+    for i in xrange(0,len(query_data['course_id'])):
+        html_string+="<tr><td>{0}</td><td>{1}</td></tr>".format(query_data['course_id'][i],query_data['count'][i])
+    html_string+="</table>"
+    log.debug(html_string)
+    return html_string
+
