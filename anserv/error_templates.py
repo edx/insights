9c04ede211769e893cc0226f93a6b42cbca7210a
Vik Paruchuri <vik@edx.org>
2013-02-22 12:51:25 -0500

    Fix up error templates

--- anserv/error_templates.py
+++ anserv/error_templates.py
@@ -2,8 +2,8 @@ from django.http import HttpResponseNotFound, HttpResponseServerError
 from mitxmako.shortcuts import render_to_string, render_to_response
 
 def render_404(request):
-    return HttpResponseNotFound(render_to_string('templates/404.html', {}))
+    return HttpResponseNotFound(render_to_string('404.html', {}))
 
 
 def render_500(request):
-    return HttpResponseServerError(render_to_string('templates/server-error.html', {}))
+    return HttpResponseServerError(render_to_string('server-error.html', {}))
