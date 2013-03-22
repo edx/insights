ef2223068cd6108fbe69ec8ae2fa9b13c6ba0021
Vik Paruchuri <vik@edx.org>
2013-03-22 11:49:00 -0400

    Fix htsql error

--- anserv/htsql_django/__init__.py
+++ anserv/htsql_django/__init__.py
@@ -14,37 +14,6 @@ DEFAULT_CONFIG = {
         'tweak.django': {},
 }
 
-engine = getattr(settings, 'DATABASES', {})
-engine = engine['default']
-
-engine_val = {
-    'sqlite3' : 'sqlite',
-    'mysql' : 'mysql',
-}
-
-engine_string = engine["ENGINE"].split(".")[3]
-instance_config = {
-    'engine' : engine_val[engine_string],
-    'username' : engine['USER'],
-    'password' : engine['PASSWORD'],
-    'host' : engine['HOST'],
-    'port' : engine['PORT'],
-    'database' : engine['NAME']
-}
-
-if instance_config['engine'] == "sqlite":
-    instance_config['database'] = os.path.abspath(instance_config['database'])
-
-if isinstance(instance_config['port'], basestring):
-    try:
-        instance_config['port'] = int(instance_config['port'])
-    except:
-        instance_config['port'] = 0
-
-log.debug(instance_config)
-log.debug(CONFIG)
-log.debug(DEFAULT_CONFIG)
-
-instance = HTSQL(instance_config, CONFIG, DEFAULT_CONFIG)
+instance = HTSQL(None, CONFIG, DEFAULT_CONFIG)
 produce = instance.produce
 
