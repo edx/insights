fde2a7a364db34f939f2ca5aba03c79c2c5e3be2
Vik Paruchuri <vik@edx.org>
2013-03-25 17:20:01 -0400

    Try changing content type

--- anserv/frontend/views.py
+++ anserv/frontend/views.py
@@ -33,8 +33,7 @@ def protected_data(request, **params):
         path = request.GET.get('path', None)
     response = HttpResponse()
     filename_suffix = path.split('.')[-1]
-    response['Content-Type'] = 'application/{0}'.format(filename_suffix)
-    response['Content-Disposition'] = 'attachment; filename={0}'.format(path)
+    response['Content-Type']=""
     log.debug("{0}{1}".format(settings.NGINX_PROTECTED_DATA_URL, path))
     response['X-Accel-Redirect'] = "{0}{1}".format(settings.NGINX_PROTECTED_DATA_URL, path)
     return response
