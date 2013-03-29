1db4d9959f0a5c9143fa63901d8e7f83a59a1853
Piotr Mitros <pmitros@mitx.mit.edu>
2013-03-29 16:17:54 -0400

    setup.py works; split package works

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import find_packages
 
 setup(name='djanalytics', 
       version='0.0',
-      package_dir={'djanalytics.an_evt' : 'src/an_evt'}, 
+      package_dir={'djanalytics':'src', 
+        'djanalytics.an_evt' : 'src/an_evt'}, 
       packages=['djanalytics', 'djanalytics.an_evt'],
       author="Piotr Mitros, Vik Paruchuri", 
       license="AGPLv3, see LICENSE.txt")
