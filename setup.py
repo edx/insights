5feabec3c2b1405f708189a2d3e7ee47c0e3af82
Piotr Mitros <pmitros@mitx.mit.edu>
2013-03-30 15:48:04 -0400

    setup.py fixed

--- setup.py
+++ setup.py
@@ -3,8 +3,7 @@ from setuptools import find_packages
 
 setup(name='djanalytics', 
       version='0.0',
-      package_dir={'djanalytics':'src', 
-        'djanalytics.djanalytics' : 'src/djanalytics'}, 
-      packages=['djanalytics', 'djanalytics.djanalytics'],
+      package_dir={'djanalytics':'src/djanalytics'}, 
+      packages=['djanalytics', 'djanalytics.core', 'djanalytics.modulefs'],
       author="Piotr Mitros, Vik Paruchuri", 
       license="AGPLv3, see LICENSE.txt")
