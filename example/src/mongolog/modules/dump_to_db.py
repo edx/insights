804147c033f3435a6dd4d67f5ad8ea43d44be387
Piotr Mitros <pmitros@mitx.mit.edu>
2013-06-02 12:35:10 -0400

    Finished rename in example

--- example/src/mongolog/modules/dump_to_db.py
+++ example/src/mongolog/modules/dump_to_db.py
@@ -3,7 +3,7 @@ import sys
 from djanalytics.core.decorators import event_handler
 
 @event_handler()
-def dump_to_db(db, events):
+def dump_to_db(mongodb, events):
     ## TODO: Error handling
-    collection = db['event_log']
+    collection = mongodb['event_log']
     collection.insert([e.event for e in events])
