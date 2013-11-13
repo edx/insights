f8be0482aff1d8e6203e88449782f787e64e9860
Brian Wilson <brian@edx.org>
2013-11-13 18:15:58 -0500

    Add version number to setup.py.

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from distutils.core import setup
 from setuptools import find_packages
 
-setup(name = 'edinsights', 
-      version = '0.0',
-      package_dir = {'edinsights':'src/edinsights'}, 
-      packages = ['edinsights', 'edinsights.core', 'edinsights.modulefs', 'edinsights.modules', 'edinsights.modules.testmodule'],
-      author="Piotr Mitros, Vik Paruchuri", 
-      data_files = [("edinsights/modules/testmodule/static/", ["src/edinsights/modules/testmodule/static/hello.html"]), 
-                    ("edinsights/modules/testmodule/templates/", ["src/edinsights/modules/testmodule/templates/hello.html"])],
-      license = "AGPLv3, see LICENSE.txt")
+setup(
+    name = 'edinsights', 
+    version = '0.1',
+    description='edX Insights Analytics Framework',
+    package_dir = {'edinsights':'src/edinsights'}, 
+    packages = ['edinsights', 'edinsights.core', 'edinsights.modulefs', 'edinsights.modules', 'edinsights.modules.testmodule'],
+    author="Piotr Mitros, Vik Paruchuri", 
+    data_files = [
+        ("edinsights/modules/testmodule/static/", ["src/edinsights/modules/testmodule/static/hello.html"]), 
+        ("edinsights/modules/testmodule/templates/", ["src/edinsights/modules/testmodule/templates/hello.html"])
+    ],
+    license = "AGPLv3, see LICENSE.txt"
+)
